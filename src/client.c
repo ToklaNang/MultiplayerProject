fcefc3725a502d18de9c80a950f29faa39a52297
NangTokla <publackgaming@gmail.com>
2026-08-13 14:58:57 +0700

    Fix a huge bug, multiplayer finally work

--- src/client.c
+++ src/client.c
@@ -20,23 +20,97 @@ struct drawQueue {
   int         tail, head;
 };
 
+playerState queuePop(drawQueue *q);
+void        queuePush(drawQueue  *q, playerState x);
+void        queueClear(drawQueue *q);
+bool        queueEmpty(drawQueue *q);
+
+void  terminateClient();
+int   consoleCtrlHandler(unsigned long ctrlType);
+void *handleClientConnection(void *args);
+
 eventInfo currEvent;
 uint32_t  assignedId        = 0;
 SOCKET    client            = INVALID_SOCKET;
 bool      clientIsReady     = false;
 bool      clientShouldClose = false;
 
-vecPlayerState prevState = _vectorEmpty(vecPlayerState);
-vecPlayerState currState = _vectorEmpty(vecPlayerState);
-bool           isWriting = false;
+drawQueue       dq;
+vecPlayerState  prevState = _vectorEmpty(vecPlayerState);
+vecPlayerState  currState = _vectorEmpty(vecPlayerState);
+pthread_mutex_t isWriting;
 
-void queueClear(drawQueue *q)      { q->head = q->tail = 0; }
-bool queueEmpty(drawQueue *q)      { return q->head == q->tail; }
-playerState queuePop(drawQueue *q) { return q->items[q->head++ % 256]; }
+int main(int argc, char **argv)
+{
+  if (argc < 3) {
+    fprintf(stderr, "ERROR: Expected an argument (ip port)\n");
+    return -1;
+  }
+  SetConsoleCtrlHandler(consoleCtrlHandler, true);
+
+  struct threadArgs args;
+  args.ipAddr = argv[1];
+  args.port   = argv[2];
+
+  // Create another thread to handle client->server connection
+  // while the main thread handle main game
+  pthread_t worker;
+  pthread_create(&worker, NULL, handleClientConnection, (void*)&args);
+
+  SetTraceLogLevel(LOG_WARNING);
+  InitWindow(CLIENT_WINDOW_WIDTH, CLIENT_WINDOW_HEIGHT, "Client");
+  SetTargetFPS(60);
+
+  clientIsReady = true;
+
+  RenderTexture gameCanvas = LoadRenderTexture(CLIENT_WINDOW_WIDTH, CLIENT_WINDOW_HEIGHT);
+  SetTextureFilter(gameCanvas.texture, TEXTURE_FILTER_POINT);
+
+  while (!clientShouldClose && !WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    currEvent.leftKeyDown  = (IsKeyDown(KEY_A))     ? true : false;
+    currEvent.rightKeyDown = (IsKeyDown(KEY_D))     ? true : false;
+    currEvent.jmpKeyDown   = (IsKeyDown(KEY_SPACE)) ? true : false;
+    
+    pthread_mutex_lock(&isWriting);
+    for (int n = 0; n < currState.len; n++) {
+      playerState ps = _at(currState, n);
+
+      DrawRectangleV(ps.position, ps.scale, ps.color);
+    }
+    pthread_mutex_unlock(&isWriting);
+
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+  terminateClient();
+
+  pthread_join(worker, NULL);
+  return 0;
+}
+void queueClear(drawQueue *q)      
+{ 
+  q->head = q->tail = 0;
+}
+bool queueEmpty(drawQueue *q)
+{ 
+  return q->head == q->tail; 
+}
+playerState queuePop(drawQueue *q) 
+{ 
+  return q->items[q->head++ % 256];
+}
 void queuePush(drawQueue  *q, playerState x)
 {
   if (q->tail - q->head > 256) return;
   q->items[q->tail++ % 256] = x;
+}
+int consoleCtrlHandler(unsigned long ctrlType)
+{
+  terminateClient();
+  return true; // Tell window that it's handled
 }
 void terminateClient()
 {
@@ -102,22 +176,24 @@ void *handleClientConnection(void *args)
       break;
     }
     case PKT_WORLD_SNAP: {
-      isWriting = true;
-      
+      pthread_mutex_lock(&isWriting);
+
+      vecPlayerState tmp = currState;
+      currState          = prevState;
+      prevState          = tmp;
+
       int    amount = p.payloadSize / sizeof(playerState);
       size_t offset = 0;
 
-      _vecDes(currState);
+      _vecDes(prevState);
       for (int n = 0; n < amount; n++) {
-        playerState state = unserializePlayer(p.payload + offset);
-        _pushBack(currState, state);
+        playerState state  = unserializePlayer(p.payload + offset);
+        offset            += sizeof(playerState);
+
+        _pushBack(prevState, state);
       }
 
-      isWriting = false;
-
-      _vecDes(prevState);
-      for (int n = 0; n < currState.len; n++)
-        _pushBack(prevState, _at(currState, n));
+      pthread_mutex_unlock(&isWriting);
       
       break;
     }
@@ -127,68 +203,4 @@ void *handleClientConnection(void *args)
 
   WSACleanup();
   return NULL;
-}
-int consoleCtrlHandler(unsigned long ctrlType)
-{
-  terminateClient();
-  return true; // Tell window that it's handled
-}
-
-int main(int argc, char **argv)
-{
-  if (argc < 3) {
-    fprintf(stderr, "ERROR: Expected an argument (ip port)\n");
-    return -1;
-  }
-  SetConsoleCtrlHandler(consoleCtrlHandler, true);
-
-  struct threadArgs args;
-  args.ipAddr = argv[1];
-  args.port   = argv[2];
-
-  // Create another thread to handle client->server connection
-  // while the main thread handle main game
-  pthread_t worker;
-  pthread_create(&worker, NULL, handleClientConnection, (void*)&args);
-
-  SetTraceLogLevel(LOG_WARNING);
-  InitWindow(CLIENT_WINDOW_WIDTH, CLIENT_WINDOW_HEIGHT, "Client");
-  SetTargetFPS(60);
-
-  clientIsReady = true;
-
-  RenderTexture gameCanvas = LoadRenderTexture(CLIENT_WINDOW_WIDTH, CLIENT_WINDOW_HEIGHT);
-  SetTextureFilter(gameCanvas.texture, TEXTURE_FILTER_POINT);
-
-  while (!WindowShouldClose() && !clientShouldClose) {
-    BeginDrawing();
-    ClearBackground(WHITE);
-
-    currEvent.leftKeyDown  = (IsKeyDown(KEY_A))     ? true : false;
-    currEvent.rightKeyDown = (IsKeyDown(KEY_D))     ? true : false;
-    currEvent.jmpKeyDown   = (IsKeyDown(KEY_SPACE)) ? true : false;
-
-    // If the world states is updating, fall back
-    // to draw using the previus state instead to
-    // avoid reading/writing at the same time
-    if (isWriting) {
-      for (int n = 0; n < prevState.len; n++) {
-        playerState ps = _at(prevState, n);
-
-        DrawRectangleV(ps.position, ps.scale, ps.color);
-      }
-    } else {
-      for (int n = 0; n < currState.len; n++) {
-        playerState ps = _at(currState, n);
-
-        DrawRectangleV(ps.position, ps.scale, ps.color);
-      }
-    }
-
-    EndDrawing();
-  }
-  terminateClient();
-
-  pthread_join(worker, NULL);
-  return 0;
 }
--- src/server.c
+++ src/server.c
@@ -9,162 +9,18 @@
 #define CLIENT_TIMEOUT_INTERVAL 300
 
 #define GRAVITY  9.81f
-#define AIR_DRAG 0.99f
 #define FRICTION 0.85f
+
+int   consoleCtrlHandler(unsigned long ctrlType);
+void  handleClientPacket(packetInfo p, SOCKADDR_IN sender, int len);
+void  playerLoop(playerInfo *p, double deltaTime);
+void *initiateServer(void *args);
 
 uint32_t      serverIdCounter   = 1;
 uint32_t      serverCurrTick    = 0;
 bool          serverShouldClose = false;
 SOCKET        server            = INVALID_SOCKET;
 vecPlayerInfo players           = _vectorEmpty(vecPlayerInfo);
-
-void handleClientPacket(packetInfo p, SOCKADDR_IN sender, int len)
-{ 
-  const char *ipAddr = inet_ntoa(sender.sin_addr);
-
-  switch (p.type) {
-  case PKT_REQUEST_JOIN: {
-    bool shouldJoin = true;
-    
-    // I don't have scenario in mind right now
-    // that the server have to reject the client
-    // so for now it's just always gonna accept
-
-    if (shouldJoin) {
-      playerInfo newPlayer;
-      newPlayer.id             = serverIdCounter++;
-      newPlayer.lastSeen       = serverCurrTick;
-      newPlayer.state.onGround = false;
-      newPlayer.ipAddr         = sender;
-      newPlayer.state.velocity = (Vector2){  0.0f,   0.0f};
-      newPlayer.state.position = (Vector2){  0.0f,   0.0f};
-      newPlayer.state.scale    = (Vector2){100.0f, 100.0f};
-      newPlayer.state.color    = (Color)  {rand() % 255, rand() % 255, rand() % 255, 255};
-
-      _pushBack(players, newPlayer);
-
-      uint32_t idNet = htonl(newPlayer.id);
-      packetInfo p   = packetServer(PKT_ACCEPT_JOIN, (char*)&idNet, sizeof(idNet));
-      sendPacketTo(server, &sender, len, p);
-      
-      printf("SERVER: Player %d joined [%s]\n", newPlayer.id, ipAddr);
-    } else {
-      packetInfo p = packetServer(PKT_REJECT_JOIN, "Server rejected", 16);
-      sendPacketTo(server, &sender, len, p);
-    }
-
-    break;
-  }
-  case PKT_PLAYER_UPDATE: {
-    for (int n = 0; n < players.len; n++) {
-      playerInfo currP = _at(players, n);
-
-      if (currP.id != p.clientId) continue;
-      
-      players.items[n].event    = unserializeEvent(p.payload);
-      players.items[n].lastSeen = serverCurrTick;
-    }
-    break;
-  }
-  case PKT_PLAYER_LEFT: {
-    for (int n = 0; n < players.len; n++) {
-      playerInfo currP = _at(players, n);
-
-      if (currP.id != p.clientId) continue;
-
-      printf("SERVER: Player %d left [%s]\n", currP.id, ipAddr);
-      _popIndex(players, n);
-    }
-    break;
-  }
-  default: break;  
-  }
-}
-void *initiateServer(void *args)
-{
-  WSADATA wsaData;
-
-  int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-  if (iResult != 0) {
-    fprintf(stderr, "ERROR: Failed to initialize winsock\n");
-    serverShouldClose = true;
-    return NULL;
-  }
-
-  server = createSocket(NULL, (char*)args);
-  if (server == INVALID_SOCKET) {
-    fprintf(stderr, "ERROR: Failed to create socket\n");
-    
-    serverShouldClose = true;
-    WSACleanup();
-    return NULL;
-  }
-  printf("INFO: Server bound to port %s\n", (char*)args);
-
-  packetInfo p;
-
-  while (!serverShouldClose) {
-    // Handle client->server connection
-    memset(&p, 0, sizeof(p));
-
-    SOCKADDR_IN sender;
-    int         len = sizeof(sender);
-
-    int byteRecv = recvPacketFrom(server, &sender, &len, &p);
-    if (byteRecv <= 0) continue;
-    
-    handleClientPacket(p, sender, len);
-  }
-
-  WSACleanup();
-}
-void playerLoop(playerInfo *p, double deltaTime)
-{
-  // Register keystroke event
-  if (p->event.leftKeyDown && p->state.onGround)
-    p->state.velocity.x -= 100.f;
-  
-  if (p->event.rightKeyDown && p->state.onGround)
-    p->state.velocity.x += 100.0f;
-
-  if (p->event.jmpKeyDown && p->state.onGround)
-    p->state.velocity.y = -450.0f;
-
-  // Physic
-  p->state.velocity.y += GRAVITY;
-  p->state.velocity.x *= (p->state.onGround)
-    ? FRICTION : AIR_DRAG;
-
-  Vector2 step = Vector2Scale(p->state.velocity, deltaTime);
-  
-  if (p->state.position.y + p->state.scale.y + step.y >= CLIENT_WINDOW_HEIGHT) {
-    float   penetration = (p->state.position.y + p->state.scale.y + step.y) - CLIENT_WINDOW_HEIGHT;
-    Vector2 floorNorm   = {0.0f, -1.0f};
-
-    step = Vector2Add(step, Vector2Scale(floorNorm, penetration));
-
-    p->state.velocity.y = 0.0f;
-    p->state.onGround   = true;
-  } else
-    p->state.onGround = false;
-
-  p->state.position = Vector2Add(p->state.position, step);
-}
-int consoleCtrlHandler(unsigned long ctrlType)
-{
-  for (int n = 0; n < players.len; n++) {
-    playerInfo currP = _at(players, n);
-
-    SOCKADDR_IN addr = currP.ipAddr;
-    int         len  = sizeof(addr);
-
-    sendPacketTo(server, &addr, len, packetServer(PKT_FORCE_CLOSE, NULL, 0));
-  }
-
-  closesocket(server);
-  serverShouldClose = true;
-  return true; // Tell window that it's handled
-}
 
 int main(int argc, char **argv)
 {
@@ -205,8 +61,10 @@ int main(int argc, char **argv)
         playerInfo currP = _at(players, n);
 
         if (serverCurrTick - currP.lastSeen >= CLIENT_TIMEOUT_INTERVAL) {
-          const char *ipAddr = inet_ntoa(currP.ipAddr.sin_addr);
-          printf("SERVER: Player %d got kicked [%s]\n", currP.id, ipAddr);
+          const char    *ipAddr  = inet_ntoa(currP.ipAddr.sin_addr);
+          const u_short  srcPort = ntohs(currP.ipAddr.sin_port);
+        
+          printf("SERVER: Player %d got kicked [%s:%d]\n", currP.id, ipAddr, srcPort);
 
           _popIndex(players, n);
           n = 0; // Reset and look again
@@ -214,11 +72,11 @@ int main(int argc, char **argv)
       }
 
       // Send snapshot of the world to each player
-      char payload[sizeof(playerState) * players.len];
-        
+      char   payload[sizeof(playerState) * players.len];
+      size_t offset = 0;
+
       for (int n = 0; n < players.len; n++) {
         playerState pState = _at(players, n).state;
-        size_t      offset = 0;
 
         serializePlayer(payload + offset, pState);
         offset += sizeof(pState);
@@ -227,11 +85,7 @@ int main(int argc, char **argv)
 
       for (int n = 0; n < players.len; n++) {
         playerInfo currP = _at(players, n);
-
-        SOCKADDR_IN to  = currP.ipAddr;
-        int         len = sizeof(to);
-
-        sendPacketTo(server, &to, len, p);
+        sendPacketTo(server, &currP.ipAddr, sizeof(currP.ipAddr), p);
       }
 
       // Perform player loop (physics, ...)
@@ -245,4 +99,161 @@ int main(int argc, char **argv)
 
   pthread_join(worker, NULL);
   return 0;
+}
+int consoleCtrlHandler(unsigned long ctrlType)
+{
+  for (int n = 0; n < players.len; n++) {
+    playerInfo currP = _at(players, n);
+
+    SOCKADDR_IN addr = currP.ipAddr;
+    int         len  = sizeof(addr);
+
+    sendPacketTo(server, &addr, len, packetServer(PKT_FORCE_CLOSE, NULL, 0));
+  }
+
+  closesocket(server);
+  serverShouldClose = true;
+  return true; // Tell window that it's handled
+}
+void handleClientPacket(packetInfo p, SOCKADDR_IN sender, int len)
+{ 
+  const char    *ipAddr  = inet_ntoa(sender.sin_addr);
+  const u_short  srcPort = ntohs(sender.sin_port);
+
+  switch (p.type) {
+  case PKT_REQUEST_JOIN: {
+    bool shouldJoin = true;
+
+    for (int n = 0; n < players.len; n++) {
+      playerInfo currP = _at(players, n);
+
+      const char    *pAddr = inet_ntoa(currP.ipAddr.sin_addr);
+      const u_short  pPort = ntohs(currP.ipAddr.sin_port);
+
+      if (strcmp(pAddr, ipAddr) != 0 || pPort != srcPort)
+        continue;
+
+      shouldJoin = false;
+      break;
+    }
+
+    if (shouldJoin) {
+      playerInfo newPlayer;
+      newPlayer.id             = serverIdCounter++;
+      newPlayer.lastSeen       = serverCurrTick;
+      newPlayer.state.onGround = false;
+      newPlayer.ipAddr         = sender;
+      newPlayer.state.velocity = (Vector2){  0.0f,   0.0f};
+      newPlayer.state.position = (Vector2){  0.0f,   0.0f};
+      newPlayer.state.scale    = (Vector2){100.0f, 100.0f};
+      newPlayer.state.color    = (Color)  {rand() % 255, rand() % 255, rand() % 255, 255};
+
+      _pushBack(players, newPlayer);
+
+      uint32_t idNet = htonl(newPlayer.id);
+      packetInfo p   = packetServer(PKT_ACCEPT_JOIN, (char*)&idNet, sizeof(idNet));
+      sendPacketTo(server, &sender, len, p);
+        
+      printf("SERVER: Player %d joined [%s:%d]\n", newPlayer.id, ipAddr, srcPort);
+    } else {
+      char kickMsg[] = "Player with the same ip is already in server";
+      packetInfo p = packetServer(PKT_REJECT_JOIN, kickMsg, sizeof(kickMsg));
+
+      sendPacketTo(server, &sender, len, p);
+    }
+    break;
+  }
+  case PKT_PLAYER_UPDATE: {
+    for (int n = 0; n < players.len; n++) {
+      playerInfo currP = _at(players, n);
+
+      if (currP.id != p.clientId) continue;
+      
+      players.items[n].event    = unserializeEvent(p.payload);
+      players.items[n].lastSeen = serverCurrTick;
+    }
+    break;
+  }
+  case PKT_PLAYER_LEFT: {
+    for (int n = 0; n < players.len; n++) {
+      playerInfo currP = _at(players, n);
+
+      if (currP.id != p.clientId) continue;
+
+      printf("SERVER: Player %d left [%s:%d]\n", currP.id, ipAddr, srcPort);
+      _popIndex(players, n);
+    }
+    break;
+  }
+  default: break;  
+  }
+}
+void playerLoop(playerInfo *p, double deltaTime)
+{
+  // Register keystroke event
+  if (p->event.leftKeyDown)
+    p->state.velocity.x -= 70.f;
+  
+  if (p->event.rightKeyDown)
+    p->state.velocity.x += 70.0f;
+
+  if (p->event.jmpKeyDown && p->state.onGround)
+    p->state.velocity.y = -450.0f;
+
+  // Physic
+  p->state.velocity.y += GRAVITY;
+  p->state.velocity.x *= FRICTION;
+
+  Vector2 step = Vector2Scale(p->state.velocity, deltaTime);
+  
+  if (p->state.position.y + p->state.scale.y + step.y >= CLIENT_WINDOW_HEIGHT) {
+    float   penetration = (p->state.position.y + p->state.scale.y + step.y) - CLIENT_WINDOW_HEIGHT;
+    Vector2 floorNorm   = {0.0f, -1.0f};
+
+    step = Vector2Add(step, Vector2Scale(floorNorm, penetration));
+
+    p->state.velocity.y = 0.0f;
+    p->state.onGround   = true;
+  } else
+    p->state.onGround = false;
+
+  p->state.position = Vector2Add(p->state.position, step);
+}
+void *initiateServer(void *args)
+{
+  WSADATA wsaData;
+
+  int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != 0) {
+    fprintf(stderr, "ERROR: Failed to initialize winsock\n");
+    serverShouldClose = true;
+    return NULL;
+  }
+
+  server = createSocket(NULL, (char*)args);
+  if (server == INVALID_SOCKET) {
+    fprintf(stderr, "ERROR: Failed to create socket\n");
+    
+    serverShouldClose = true;
+    WSACleanup();
+    return NULL;
+  }
+  printf("INFO: Server bound to port %s\n", (char*)args);
+
+  packetInfo p;
+
+  while (!serverShouldClose) {
+    // Handle client->server connection
+    memset(&p, 0, sizeof(p));
+
+    SOCKADDR_IN sender;
+    int         len = sizeof(sender);
+
+    int byteRecv = recvPacketFrom(server, &sender, &len, &p);
+    if (byteRecv <= 0) continue;
+    
+    handleClientPacket(p, sender, len);
+  }
+
+  WSACleanup();
 }
